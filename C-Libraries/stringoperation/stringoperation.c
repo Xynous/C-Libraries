e9a68682553d7053fb78491d3cbc7d085bfeb24a
Ashleigh Day <day.ashleigh@gmail.com>
2019-12-09 21:50:55 +0000

    Within this commit, their is a few C++ functios which have been ported to the C programming language. Further work will need to be done to port any remaining functions to the C programming language.

--- C-Libraries/stringoperation/stringoperation.c
+++ C-Libraries/stringoperation/stringoperation.c
@@ -1,1 +1,107 @@
-// C stringoperation source file
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+char *trim_left(char *stringValue) {
+	int index = 0;
+	int i;
+
+	while (stringValue[index] == ' ' || stringValue[index] == '\t'
+			|| stringValue[index] == '\n') {
+		index++;
+	}
+
+	if (index != 0)	// If no leading whitespaces been found, then no trimming operation is required.
+			{
+		i = 0;
+
+		while (stringValue[i + index] != '\0') {
+			stringValue[i] = stringValue[i + index];
+			i++;
+		}
+
+		stringValue[i] = '\0';
+	}
+
+	return stringValue;
+}
+
+char *trim_right(char *stringValue) {
+	int stringLength;
+
+	stringLength = strlen(stringValue) - 1;
+
+	while (stringLength > -1) {
+		if (stringValue[stringLength] == ' '
+				|| stringValue[stringLength] == '\t') {
+			stringLength--;
+		} else {
+			break;
+		}
+	}
+	stringValue[stringLength + 1] = '\0';
+
+	return stringValue;
+
+}
+
+char *toLowerCase(char *pCharactersToLower) {		// Need to test code further with Linux
+
+	char* toLowerCaseReturn;
+	errno_t error;
+
+	error = _strlwr_s(toLowerCaseReturn = _strdup(pCharactersToLower), strlen(pCharactersToLower) + 1);
+
+	return toLowerCaseReturn;
+	
+}
+
+char *toUpperCase(char *pCharactersToUpper) {	// Need to test code further with Linux
+
+	char* toUpperCaseReturn;
+	errno_t error;
+
+	error = _strupr_s(toUpperCaseReturn = _strdup(pCharactersToUpper), strlen(pCharactersToUpper) + 1);
+
+	return toUpperCaseReturn;
+}
+
+char *trim(char *stringValue, char *trimmingOption) {
+	
+	char *errorMessage;
+	
+	if (strcmp(trimmingOption, "L") == 0) {
+
+		stringValue = trim_left(stringValue);
+
+	}
+
+	if (strcmp(trimmingOption, "R") == 0) {
+
+		stringValue = trim_right(stringValue);
+
+	}
+
+	if (strcmp(trimmingOption, "A") == 0) {
+
+		stringValue = trim_left(stringValue);
+		stringValue = trim_right(stringValue);
+
+	}
+
+	if (trimmingOption != "L" && trimmingOption != "R" && trimmingOption != "A") {		// Condition checks if the trimming operation is valid.
+
+		errorMessage = "Error: Invalid trimming operation, refer to the project wiki for the list of trimming operations";
+
+		return errorMessage;
+	}
+
+	if (strcmp(stringValue, "") == 0) {		// Condition checks if the passed string value is empty.
+
+		errorMessage = "Error: String cant be empty, refer to the project wiki for further information and examples";
+
+		return errorMessage;
+	}
+
+	return stringValue;
+}
--- C-Libraries/stringoperation/stringoperation.h
+++ C-Libraries/stringoperation/stringoperation.h
@@ -1,1 +1,31 @@
-// C stringoperation header (.h)
+/*
+ * stringoperation.h
+ *
+ *  Created on: 5 Dec 2019
+ *      Author: ashleigh
+ */
+
+#ifndef STRINGOPERATION_H_
+#define STRINGOPERATION_H_
+
+#endif /* STRINGOPERATION_H_ */
+
+
+/* char *toLowerCase(char *);
+
+char *toUpperCase();
+
+int size();
+
+char *split();
+
+bool findString();
+
+int find();
+
+char *trim(); */
+
+
+
+
+
